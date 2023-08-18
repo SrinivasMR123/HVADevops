2b6c44949f29093e54483511eae8553deb83ce99
SrinivasMR123 <131585969+SrinivasMR123@users.noreply.github.com>
2023-08-18 21:21:51 +0530

    Add files via upload

---
+++ Raindrops.py
@@ -0,0 +1,18 @@
+def raindrops():
+    number = int(input(""))
+    result = ""
+
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+
+    if not result:
+        result = str(number)
+
+    return result
+
+
+print(raindrops())
